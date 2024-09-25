8fa714e381cecdcc745ebcc43d8f0650410c86e8
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-09-25 22:46:19 +0200

    update settings

--- DentySmile_API/settings.py
+++ DentySmile_API/settings.py
@@ -172,7 +172,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
 }
